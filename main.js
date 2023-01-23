2bbff108f589ff6cbc2f4c0d5c5f2955e4f04e7a
abualbushr <afabunn9@gmail.com>
2023-01-23 17:02:10 +0530

    3rd

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let names = ['Habib', 'Ahmed', 'Mustafa'];
 // document.write(names);
 console.log(names);
+let login = prompt('Enter your Name:');
+window.alert('Are you sure to continue with ' + login + '?');
+if(login == 'Mustafa' || login == 'Habib' || login == 'Ahmed'){
+    document.write('Hello, ' + login);
+}else{
+    document.write('Sorry you are not logged in');
+}
